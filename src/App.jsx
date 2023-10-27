26541ffa3ba0dda5bca1ec2b22e7d0cb65430fe9
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2023-10-26 23:46:11 -0700

    added action with helper function and how layouts work

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,31 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard, { dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
 
+// Layouts
+import Main, { mainLoader } from "./layouts/Main";
+
+// Actions
+import { logoutAction } from "./actions/logout";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
-    loader: dashboardLoader,
+    element: <Main />,
+    loader: mainLoader,
     errorElement: <Error />,
+    children: [
+      {
+        // path: "/",
+        index: true,
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
+      },
+    ],
   },
 ]);
 
